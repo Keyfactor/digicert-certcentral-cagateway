1a570c071364a50356411ebe8e4d7857ef432e4e
David Galey <dgaley@keyfactor.com>
2023-09-22 14:34:20 -0400

    Allow for config to specify payment method for enrollment

--- digicert-certcentral-cagateway/API/OrderCertificate.cs
+++ digicert-certcentral-cagateway/API/OrderCertificate.cs
@@ -92,6 +92,9 @@ namespace Keyfactor.Extensions.AnyGateway.DigiCert.API
 		[JsonProperty("custom_fields")]
 		public List<MetadataField> CustomFields { get; set; }
 
+		[JsonProperty("payment_method")]
+		public string PaymentMethod { get; set; }
+
 		public void SetOrganization(int? organizationId)
 		{
 			if (organizationId.HasValue)
